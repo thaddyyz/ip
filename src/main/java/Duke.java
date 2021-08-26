12e8eebbe718cce4d9a5bf812b0b190cf6505d9c
thaddyyz <76462596+thaddyyz@users.noreply.github.com>
2021-08-26 15:04:19 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,18 +8,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
-        int x=0,y;
+        int x=0,y,l;
+        ArrayList<String> list=new ArrayList<>();
         while(x==0){
             String line;
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
             y=line.indexOf("bye");
+            l=line.indexOf("list");
             if(y!=-1){
                 System.out.println("Bye. Hope to see you again soon!");
                 x=1;
             }
+            else if(l!=-1){
+                for(int k=1;k<=list.size();k++){
+                    System.out.println(k+". "+list.get(k-1));
+                }
+            }
             else{
-                System.out.println(line);
+                list.add(line);
+                System.out.println("added: "+line);
             }
 
         }
